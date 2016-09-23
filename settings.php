603b738889ac37509a1fde2dd653301628881e7f
Shawn Joyce <shawnjoyce84@gmail.com>
2016-09-22 22:15:43 -0600

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+
+// Location of your db
+DEFINE('SQL_HOST', 'localhost');
+// Username 
+DEFINE('SQL_USER', 'username');
+// Password
+DEFINE('SQL_PASS', 'password');
+// DB table
+DEFINE('SQL_DB', 'test_table');
+// Connection error message
+DEFINE('SQL_CONNECT_ERR_MSG', 'There was an error with your connection : ');
+
+?>
